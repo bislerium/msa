585ed3b917534e6641472b0912cd6cac2aceb76c
Bishal Gharti Chhetri <bishal.chhetri@innovatetech.co>
2024-12-15 12:39:28 +0545

    error fixed

---
+++ src/BuildingBlocks/Extensions/Msa.Extensions.Random/RandomExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Msa.Extensions.Random
+{
+    public static class RandomExtensions
+    {
+        public static Task Delay(this System.Random random, int minMills = 0, int maxMills = 0)
+        {
+            if (minMills == 0 && maxMills == 0) return Task.CompletedTask;
+            var randMills = random.Next(minMills, maxMills);
+            return Task.Delay(randMills);
+        }
+    }
+}
